9febe0af7d3e75f28d60aacbcd97a129754d968b
Sarah <sarah.wolf@htldornbirn.at>
2024-12-01 12:00:58 +0100

    finished_01_JS_Vars&Ops

---
+++ 02_JavaScript/01_JS_Variablen_&_Operatoren/script.js
@@ -0,0 +1,53 @@
+/*console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den WErt " + a );
+
+//a = 10
+console.log("A hat den WErt " + a );
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+//*, /, +, -
+
+c = b-a;
+console.log("b - a = " + c);
+
+*/
+
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+console.log("die werte sind a = " + a + "; b = " + b + "; c = " + c);
+
+
+let result = a + b + c;
+console.log("a + b + c = " + result);
+
+result = (a+b)/c;
+console.log("(a+b)/c = " + result);
+
+result = c - a + b;
+console.log("c - a + b; = " + result);
+
+result = a + b + c * 3
+console.log("a + b + c * 3 = " + result);
+
+result = (a + b + c)/4;
+console.log("(a + b + c) / 4 = " + result);
+
+a = 3;
+b = a + 3;
+c = c-3;
+
+console.log("die neuen werte sind a = " + a + "; b = " + b + "; c = " + c);
+
