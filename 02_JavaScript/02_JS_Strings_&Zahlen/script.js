9715381bd5ba1269a99cc994b75dd7f012a33a6a
Sarah <sarah.wolf@htldornbirn.at>
2024-12-02 16:14:39 +0100

    finished_ex_02

---
+++ 02_JavaScript/02_JS_Strings_&Zahlen/script.js
@@ -0,0 +1,34 @@
+let a = 7;
+let b = 8;
+
+let result = a -b;
+
+console.log(result);
+console.log(result);
+
+//+, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+result = c+ d;
+console.log(result);
+
+
+//Texte STRINGS
+
+let Name ="Sarah";
+console.log(Name);
+
+console.log("das Ergebnis von " + a + " und " + b + " ist " + result);
+
+
+let e = 17.4;
+let f = 3.9;
+result = e/f;
+console.log("The result is of " +e+ " divided by " +f+ " is " + result);
+
+let firstname = "Sarah";
+let lastname = "Wolf";
+
+console.log("Ich heiße " + firstname +" "+ lastname);
