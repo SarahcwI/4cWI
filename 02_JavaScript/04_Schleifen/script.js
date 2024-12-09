fadbc94b0adc3bb38f0ed348da30a58cd8e6fe50
Sarah <sarah.wolf@htldornbirn.at>
2024-12-09 12:56:18 +0100

    finished_ex_04_loops

---
+++ 02_JavaScript/04_Schleifen/script.js
@@ -0,0 +1,16 @@
+console.log("Test");   
+
+let a = 7;
+let b = 4.3;
+let Name ="Sara";
+
+console.log(Name + " " + b + a);
+
+
+//loops 
+
+for(let count = 0; count < 10; count++){
+    console.log(count+1  + " " +Name);
+}
+
+console.log("Fertig");
