eea677ae2774a5080d84bab921d0d89fa454f2e4
Sarah <sarah.wolf@htldornbirn.at>
2024-12-09 14:01:55 +0100

    Finished_Ex_05_If_Bedingung

---
+++ 02_JavaScript/05_If_Bedingung/script.js
@@ -0,0 +1,36 @@
+// erstelle eine Variable number
+let number = 50 ;
+
+console.log("Aufgabe 1: ");
+
+if (number < 20) {
+    console.log("Mini");
+}
+
+else if (number >= 20 && number <= 50) {
+    console.log("Medium");
+}
+
+else if (number > 50){
+     console.log("Large"); 
+}
+
+
+console.log("Aufgabe 2: ");
+
+
+let number2 = 91 ;
+if(number2 <= 7){
+    console.log("Mini");
+}
+else if (number2 >= 7 && number2 <= 90){
+    console.log("Medium");
+}
+
+else if(number2 > 90){
+    console.log("Large");
+}
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
