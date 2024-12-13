6766b91ec223067a4dec5644d092932e10bf1cc2
Sarah <sarah.wolf@htldornbirn.at>
2024-12-13 10:35:43 +0100

    ex_09_finished_modulo_vid

---
+++ 02_JavaScript/09_Aufgabe_Zeichen/script.js
@@ -0,0 +1,21 @@
+let a = 8;
+let b = 2;
+
+
+if (a % 8 == 0) {
+    console.log("Nubmer is even")
+}
+else {
+    console.log("Nubmer is odd")
+}
+
+a = 1999;
+b = 2;
+
+for (let i = 1; i < 50; i++) {
+    console.log("x")
+
+    if(i%5==0){
+        console.log("")
+    }
+}
