fa1c460bafa15c1b76bb46f24b866b72bc7a78b5
Sarah <sarah.wolf@htldornbirn.at>
2025-01-12 13:38:54 +0100

    finshed_calculator_function_ex1

--- 03_HTML_&_JS/01_Zugriff_JS_HTML/script.js
+++ 03_HTML_&_JS/01_Zugriff_JS_HTML/script.js
@@ -1,12 +1,32 @@
-function buttonClicked(){
-    console.log("clocked");
-
+function add(){
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-
     let result = parseFloat(num1) + parseFloat(num2);
 
-    alert(result);
-    
+    printResult.innerHTML = "Result = " + result  ;
+}
+
+function substract(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let result = parseFloat(num1) - parseFloat(num2);
+
+    printResult.innerHTML = "Result = " + result  ;
+}
+
+function multiply(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let result = parseFloat(num1) * parseFloat(num2);
+
+    printResult.innerHTML = "Result = " + result  ;
+}
+
+function divide(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let result = parseFloat(num1) / parseFloat(num2);
+
+    printResult.innerHTML = "Result = " + result  ;
 }
 
