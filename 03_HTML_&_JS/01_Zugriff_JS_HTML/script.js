ba58e0ce5587cb9a4e250d31c4de90692475d104
Sarah <sarah.wolf@htldornbirn.at>
2025-01-12 13:27:16 +0100

    finished_vid2_ex1

--- 03_HTML_&_JS/01_Zugriff_JS_HTML/script.js
+++ 03_HTML_&_JS/01_Zugriff_JS_HTML/script.js
@@ -1,4 +1,12 @@
-let element = document.getElementById("content").innerHTML = "Tolle Sace"
-console.log(element);
+function buttonClicked(){
+    console.log("clocked");
 
-alert(element);
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let result = parseFloat(num1) + parseFloat(num2);
+
+    alert(result);
+    
+}
+
