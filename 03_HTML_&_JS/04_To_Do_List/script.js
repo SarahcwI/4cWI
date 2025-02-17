5f6a8444f9ae59abd4f586193b1c03e2d756c5a8
Sarah <sarah.wolf@htldornbirn.at>
2025-02-17 15:42:22 +0100

    add_input_to_console

--- 03_HTML_&_JS/04_To_Do_List/script.js
+++ 03_HTML_&_JS/04_To_Do_List/script.js
@@ -1,14 +1,25 @@
 let todos = [
-    {id: 1, title: "putzen", isDone: false},
-    {id: 2, title: "Aufgaben", isDone: false},
-    {id: 3, title: "Essen", isDone: false},
+    { id: 1, title: "putzen", isDone: false },
+    { id: 2, title: "Aufgaben", isDone: false },
+    { id: 3, title: "Essen", isDone: false },
 ]
 
 
-let html = "";
-
-todos.forEach(element => {
-    html+="<div>" + element.title + " - " + element.isDone + "</div>"; 
+document.getElementById("input").addEventListener("change", function (event) {
+    console.log(event.target.value);
 });
 
-document.getElementById("todos").innerHTML = html;
+
+
+function printTasks() {
+    let html = "";
+
+    todos.forEach(element => {
+        html += "<div>" + element.title + " - " + element.isDone + "</div>";
+    });
+
+    document.getElementById("todos").innerHTML = html;
+
+}
+
+printTasks();
