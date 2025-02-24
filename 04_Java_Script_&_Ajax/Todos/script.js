80af6419a41350c52ffd952ff5b3761e2cca0c81
Sarah <sarah.wolf@htldornbirn.at>
2025-02-24 15:41:54 +0100

    finsihed_ex_TodoList

--- 04_Java_Script_&_Ajax/Todos/script.js
+++ 04_Java_Script_&_Ajax/Todos/script.js
@@ -1,14 +1,23 @@
-
-
 document.getElementById("buttonLoad").addEventListener("click", function () {
-loadData();
+    loadData();
 });
 
 function loadData() {
     fetch("https://5e391b0baad2220014962337.mockapi.io/swp/todos")
         .then((response) => response.json())
         .then((json) => {
-            console.log(json)
+            let html = [];
+
+            json.forEach(todo => {
+                html.push(
+                    "<div></div>" +
+                     todo.title + 
+                     "<div><img width = '40px'src='" +
+                      todo.image + "?id=" + Math.random() + 
+                      "'/><div>-----</div></div>");
+            });
+            document.getElementById("content").innerHTML = html.join("");
+
         })
 }
 
