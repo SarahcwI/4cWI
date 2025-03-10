636a23a2e5960f3e92480fd9f091d20339129614
Sarah <sarah.wolf@htldornbirn.at>
2025-03-10 16:32:08 +0100

    added_Card_Component

--- 06_React/01_Setup_React/demo1/src/App.jsx
+++ 06_React/01_Setup_React/demo1/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="bg-amber-300">Hello Vite + React!</h1>
+      <h1 className='bg-amber-300'>Hello Vite + React!</h1>
+      <div>Test</div>
+      <Card/>
     </>
   )
 }
