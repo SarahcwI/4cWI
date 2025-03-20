d2edc04906e57afb1ad461ee32dcf4d79926cf44
Sarah <sarah.wolf@htldornbirn.at>
2025-03-20 17:42:44 +0100

    Added_Lists_Component

---
+++ 06_React/02_Aufgabe_Komponente/Komponenten_bauen/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import List from './components/List'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <List Drink={"Coffee"} />
+        <List Drink={"Tea"} />
+        <List Drink={"Beer"} />       
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
