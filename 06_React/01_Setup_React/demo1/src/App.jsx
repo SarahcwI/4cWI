fcd6e85f084a4b5b304ccea31f281e4662d3e801
Sarah <sarah.wolf@htldornbirn.at>
2025-03-20 17:12:34 +0100

    finished_first_Component

--- 06_React/01_Setup_React/demo1/src/App.jsx
+++ 06_React/01_Setup_React/demo1/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import Card from './components/Card'
+
 
 function App() {
 
   return (
     <>
       <h1 className='bg-amber-300'>Hello Vite + React!</h1>
-      <div>Test</div>
-      <Card/>
+
+      <div className="flex">
+        <Card name={"Lenz"} jobtitel={"AV"} description={"lässig"} />
+        <Card name={"Sams"} jobtitel={"AV"} description={"lässig ebenfalls"} />
+      </div>
+
+
     </>
   )
 }
