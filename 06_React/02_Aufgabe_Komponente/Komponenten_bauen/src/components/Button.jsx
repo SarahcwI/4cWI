e4f850636687a22ea03c484bc70c0743464bd1ae
Sarah <sarah.wolf@htldornbirn.at>
2025-03-21 09:28:23 +0100

    added_color_change_&_Finished_stateVid

--- 06_React/02_Aufgabe_Komponente/Komponenten_bauen/src/components/Button.jsx
+++ 06_React/02_Aufgabe_Komponente/Komponenten_bauen/src/components/Button.jsx
@@ -3,16 +3,18 @@ import {useState} from 'react';
 
 export default function 
 Button({description}) {
-
     const[amountOfClicks, setAmountofClicks] = useState(0);
+
+    let style = amountOfClicks > 4 ? "bg-red-500 hover:bg-red-700" : "";
+
   return (
-    <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-fit">
-        <div onClick={()=>{
-            setAmountofClicks(amountOfClicks + 1);
-        }}>
-            <button>{description}</button>
-            <div>Button clicked: {amountOfClicks}</div>
-        </div>
+    <div 
+    className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-fit ${style}`}
+    onClick={()=>{
+        setAmountofClicks(amountOfClicks + 1);
+    }}>
+        <button>{description}</button>
+        <div>Button clicked: {amountOfClicks}</div>
     </div>
   )
 }
