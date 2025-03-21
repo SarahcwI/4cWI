422a915e93e990821d47f9a3d002e51d6b0a79c5
Sarah <sarah.wolf@htldornbirn.at>
2025-03-21 09:22:52 +0100

    increase_amountOfClicks_working

--- 06_React/02_Aufgabe_Komponente/Komponenten_bauen/src/components/Button.jsx
+++ 06_React/02_Aufgabe_Komponente/Komponenten_bauen/src/components/Button.jsx
@@ -7,8 +7,12 @@ Button({description}) {
     const[amountOfClicks, setAmountofClicks] = useState(0);
   return (
     <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-fit">
-        <button>{description}</button>
-        <div>Button clicked: {amountOfClicks}</div>
+        <div onClick={()=>{
+            setAmountofClicks(amountOfClicks + 1);
+        }}>
+            <button>{description}</button>
+            <div>Button clicked: {amountOfClicks}</div>
+        </div>
     </div>
   )
 }
