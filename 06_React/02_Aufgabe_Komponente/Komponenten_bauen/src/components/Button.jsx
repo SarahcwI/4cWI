c7da2e49c85b7289b9ff38a99a984df6ab4d232b
Sarah <sarah.wolf@htldornbirn.at>
2025-03-21 09:16:44 +0100

    added_use_state_to_btn_but_without_onclick

--- 06_React/02_Aufgabe_Komponente/Komponenten_bauen/src/components/Button.jsx
+++ 06_React/02_Aufgabe_Komponente/Komponenten_bauen/src/components/Button.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {useState} from 'react';
 
 export default function 
-button({description}) {
+Button({description}) {
+
+    const[amountOfClicks, setAmountofClicks] = useState(0);
   return (
     <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-fit">
         <button>{description}</button>
+        <div>Button clicked: {amountOfClicks}</div>
     </div>
   )
 }
