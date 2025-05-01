f5c34a8b32494afee40ffd2d15ca11403d226e0a
Sarah <sarah.wolf@htldornbirn.at>
2025-05-01 16:38:10 +0200

    Ex5 Stylized Page and added toLowerCase in filter | finished Ex5

--- 06_React/Demo_Project/src/components/PeopleContainer.jsx
+++ 06_React/Demo_Project/src/components/PeopleContainer.jsx
@@ -16,16 +16,16 @@ export default function PeopleContainer() {
   }, []);
 
   const filterPeople = (filter) => {
-    let filtered = people.filter(person => person.name.includes(filter));
+    let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
     setFilteredPeople(filtered);
   }
 
 
 
   return (
-    <div>
-      <h1>People</h1>
-      <div className="border">
+    <div className="">
+      <div className="border mb-4 fixed bg-white w-full h-28 p-8">
+
         <input
           className="border p-4"
           type="text"
@@ -37,7 +37,7 @@ export default function PeopleContainer() {
         />
       </div>
 
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32">
         {filteredPeople.map((person) => {
           return (
             <PersonCard
