5ec45e92f150e60545824088bf594b38fb1572f1
Sarah <sarah.wolf@htldornbirn.at>
2025-05-01 15:37:58 +0200

    Ex3_Fetch_working

--- 06_React/Demo_Project/src/components/PeopleContainer.jsx
+++ 06_React/Demo_Project/src/components/PeopleContainer.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PersonCard from './PersonCard'
 
 export default function PeopleContainer() {
+
+    useEffect(() => {
+      fetch("https://67b84587699a8a7baef35d80.mockapi.io/person").then(
+        (res) => res.json().then((data) => {
+        console.log(data);
+      })
+    );
+    }, []);
+  
+
+  
   return (
     <div className='w-40'>
         <h1>People</h1>
         <PersonCard name="Hans" titel="ceo" imageUri="https://picsum.photos/200/300"/>
+        <PersonCard name="Hans" titel="ceo" imageUri="https://picsum.photos/200/300"/>
+        <PersonCard name="Hans" titel="ceo" imageUri="https://picsum.photos/200/300"/>
     </div>
   )
 }
+
