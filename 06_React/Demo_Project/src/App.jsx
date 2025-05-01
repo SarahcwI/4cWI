e3ae78e21b62dd35b638bf3db2bb87b712633fad
Sarah <sarah.wolf@htldornbirn.at>
2025-05-01 14:57:30 +0200

    reworked_folder_structure_&_Added_Person_Component_Ex_3

---
+++ 06_React/Demo_Project/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import Components1 from "./components/pages/Components1";
+import Card1 from "./components/Card1";
+import PeopleContainer from "./components/PeopleContainer";
+
+function App() {
+  return (
+    <>
+      {/*<Components1 />
+     <Card1 />*/}
+
+     <PeopleContainer />
+    </>
+  );
+}
+
+export default App;
