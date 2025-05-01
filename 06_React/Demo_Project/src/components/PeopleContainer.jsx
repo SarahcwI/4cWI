8d67ede17d2bb020ff64da30a3d95decf6ffe84f
Sarah <sarah.wolf@htldornbirn.at>
2025-05-01 16:12:37 +0200

    Ex5 Added Input Field and onChange functionality

--- 06_React/Demo_Project/src/components/PeopleContainer.jsx
+++ 06_React/Demo_Project/src/components/PeopleContainer.jsx
@@ -1,36 +1,46 @@
-import React, { useEffect } from 'react'
-import PersonCard from './PersonCard'
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import PersonCard from "./PersonCard";
+import { useState } from "react";
 
 export default function PeopleContainer() {
+  const [people, setPeople] = useState([]);
 
-const [people, setPeople] = useState([]);
-const [color, setColor] = useState("green");
-
-    useEffect(() => {
-      fetch("https://67b84587699a8a7baef35d80.mockapi.io/person").then(
-        (res) => res.json().then((data) => {
+  useEffect(() => {
+    fetch("https://67b84587699a8a7baef35d80.mockapi.io/person").then((res) =>
+      res.json().then((data) => {
         setPeople(data);
       })
     );
-    }, []);
-  
-    useEffect(() => {
-      alert("Changed color to " + color);
-    }, [color]);
+  }, []);
 
-  
   return (
     <div>
-        <h1>People</h1>
-        <button onClick={()=>(setColor("orange"))}>Change Color</button>
-        <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-            {people.map(person => {
-              return <PersonCard name={person.name} titel={person.jobtitel} imageUri={person.avatar}/>
-              
-            })}
-        </div>
-    </div>
-  )
-}
+      <h1>People</h1>
+      <div className="border">
+        <input
+          className="border p-4"
+          type="text"
+          placeholder="Search"
+          onChange={(el) => {
+            console.log(el.target.value);
+          }}
+        />
+      </div>
 
+
+
+      <button onClick={() => setColor("orange")}>Change Color</button>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        {people.map((person) => {
+          return (
+            <PersonCard
+              name={person.name}
+              titel={person.jobtitel}
+              imageUri={person.avatar}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
