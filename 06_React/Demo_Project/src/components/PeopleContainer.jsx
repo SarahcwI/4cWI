5c430a17502a890f8ccf11a07e63468c48be029a
Sarah <sarah.wolf@htldornbirn.at>
2025-05-01 16:04:17 +0200

    Ex4 Tried out color change use state

--- 06_React/Demo_Project/src/components/PeopleContainer.jsx
+++ 06_React/Demo_Project/src/components/PeopleContainer.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 export default function PeopleContainer() {
 
 const [people, setPeople] = useState([]);
+const [color, setColor] = useState("green");
+
     useEffect(() => {
       fetch("https://67b84587699a8a7baef35d80.mockapi.io/person").then(
         (res) => res.json().then((data) => {
@@ -13,11 +15,15 @@ const [people, setPeople] = useState([]);
     );
     }, []);
   
+    useEffect(() => {
+      alert("Changed color to " + color);
+    }, [color]);
 
   
   return (
     <div>
         <h1>People</h1>
+        <button onClick={()=>(setColor("orange"))}>Change Color</button>
         <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
             {people.map(person => {
               return <PersonCard name={person.name} titel={person.jobtitel} imageUri={person.avatar}/>
